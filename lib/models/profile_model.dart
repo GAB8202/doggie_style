79091a5f3d1fd5307106797b6d692514f075fd9c
Margaret Kriz <71287192+mrkriz101@users.noreply.github.com>
2023-11-16 10:29:59 -0500

    4 profile pictures now, scrollable

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -9,5 +9,5 @@ class ProfileModel {
   String? imageAsset4;
   List<String> personalityTraits;
 
-  ProfileModel({this.dogName, this.dogAge, this.dogBreed, this.dogSize, this.imageAsset1, this.imageAsset2, this.imageAsset3, this.personalityTraits = const []});
+  ProfileModel({this.dogName, this.dogAge, this.dogBreed, this.dogSize, this.imageAsset1, this.imageAsset2, this.imageAsset3,this.imageAsset4, this.personalityTraits = const []});
 }
